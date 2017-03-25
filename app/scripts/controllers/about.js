2247a78253abba81f1a5a386aafbaaf8b2d554c8
AdrienAtallah <adrienatallah@gmail.com>
2017-03-25 14:30:25 -0400

    initialize repo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name commentBoardApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the commentBoardApp
+ */
+angular.module('commentBoardApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name commentBoardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the commentBoardApp
+ */
+angular.module('commentBoardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
